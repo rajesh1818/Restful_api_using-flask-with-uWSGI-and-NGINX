40e4b934f475dc9cad43303285d4e682d0514430
rajesh jethwa <58974729+rajesh1818@users.noreply.github.com>
2021-08-06 15:38:30 +0530

    Update __init__.py

--- master/__init__.py
+++ master/__init__.py
@@ -5,7 +5,7 @@ from flask_restful import Api
 #
 app = Flask(__name__)
 app1 = Api(app)
-app.config['MONGO_URI']="mongodb+srv://rajesh:rajesh@imdb.8hily.mongodb.net/imdb?retryWrites=true&w=majority"
+app.config['MONGO_URI']="mongodb+srv://<username>:<password>@imdb.8hily.mongodb.net/<database_name>?retryWrites=true&w=majority"
 app.config["JWT_SECRET_KEY"] = "super-secret"
 imdb = PyMongo(app)
 jwt = JWTManager(app)
